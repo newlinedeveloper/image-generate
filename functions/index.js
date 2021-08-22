46c2ce0812508c6415de018634996949978c2c6f
veerasolaiyappan <veerasolaiyappan@gmail.com>
2021-08-22 17:09:43 +0530

    heic to jpeg format

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,8 @@ const sharp = require('sharp');
 // const os = require('os');
 const {tmpdir} = require('os');
 const fs = require('fs-extra');
+const { promisify } = require('util');
+const convert = require('heic-convert');
 
 const THUMB_MAX_WIDTH = 200;
 const THUMB_MAX_HEIGHT = 200;
@@ -19,26 +21,34 @@ const THUMB_MAX_200 = 200;
 
 const THUMB_MAX_400 = 400;
 
-const THUMB_MAX_1000 = 1000;
+const THUMB_MAX_1000 = 800;
 
 
 const gcs = new Storage();
 
 
-exports.generateThumbnail = functions.storage
+exports.generateThumbnail = functions.runWith({memory: '1GB', timeoutSeconds: '360'})
+  .storage
   .object()
   .onFinalize(async object => {
-    const bucket = gcs.bucket(object.bucket);
-    const filePath = object.name;
-    const fileName = filePath.split('/').pop();
+    let bucket = gcs.bucket(object.bucket);
+    let filePath = object.name;
+    let fileName = filePath.split('/').pop();
     // let bucketDir = dirname(filePath);
+
+    let imageType = ['image','application']
+
+    let contentType = object.contentType.split("/")[0]
+
+    console.log("OBbject => "+object.contentType);
 
     let bucketDir = "thumbnails";
 
-    const workingDir = join(tmpdir(), 'thumbs');
-    const tmpFilePath = join(workingDir, 'source.png');
+    let workingDir = join(tmpdir(), 'thumbs');
+    let tmpFilePath = join(workingDir, 'source.png');
 
-    if (fileName.includes('thumb@') || !object.contentType.includes('image')) {
+    // if (fileName.includes('thumb@') || !object.contentType.includes('image') || !object.contentType.includes('application/octet-stream')) {
+    if (fileName.includes('thumb@') || imageType.indexOf(contentType) == -1) {
       console.log('exiting function');
       return false;
     }
@@ -56,21 +66,42 @@ exports.generateThumbnail = functions.storage
 
     let sizes = [THUMB_MAX_200, THUMB_MAX_400];
 
-    if (object.size >= 1000000){
+    if (object.size >= 500000){
       sizes.push(THUMB_MAX_1000);
     }
+
+    if(object.contentType.includes('octet-stream') || object.contentType.includes('heic')){
+
+     
+      const inputBuffer = await promisify(fs.readFile)(tmpFilePath);
+      const outputBuffer = await convert({
+        buffer: inputBuffer, // the HEIC file buffer
+        format: 'JPEG',      // output format
+        quality: 1           // the jpeg compression quality, between 0 and 1
+      });
+      fileName=fileName.replace('heic','jpeg');
+
     
+      await promisify(fs.writeFile)(tmpFilePath, outputBuffer);
+
+      
+      
+    }
+   
 
     const uploadPromises = sizes.map(async size => {
 
-      const thumbName = `thumb@${size}_${fileName}`;
-      const thumbPath = join(workingDir, thumbName);
+      let thumbName = `thumb@${size}_${fileName}`;
+      let thumbPath = join(workingDir, thumbName);
 
-      // Resize source image
+    // Sharp docs  https://sharp.pixelplumbing.com/api-resize
       await sharp(tmpFilePath)
-        .resize(size, size)
+        .resize(size, size,{
+          fit: sharp.fit.inside,
+          withoutEnlargement: true
+        })
         .toFile(thumbPath);
-
+      
         bucketDir = "thumbnails"+size.toString();
 
       // Upload to GCS
