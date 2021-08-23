f06d4f4fee9dd5b43c51c80f350b7e666f652a38
veerasolaiyappan <veerasolaiyappan@gmail.com>
2021-08-23 22:33:53 +0530

    Firebase database log update

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@
 const functions = require('firebase-functions');
 const { Storage } = require('@google-cloud/storage');
 const admin = require('firebase-admin');
-admin.initializeApp();
+// admin.initializeApp();
 // const path = require('path');
 const { join, dirname } = require('path');
 const sharp = require('sharp');
@@ -24,7 +24,30 @@ const THUMB_MAX_400 = 400;
 const THUMB_MAX_1000 = 800;
 
 
-const gcs = new Storage();
+// const gcs = new Storage();
+
+
+
+var serviceAccount = require("./images-service-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://images-5d13c-default-rtdb.firebaseio.com/"
+});
+
+const firestore = admin.firestore();
+const db = admin.database();
+
+
+var config = {
+  projectId: 'images-5d13c',
+  keyFilename: './images-service-key.json',
+  credentials: serviceAccount
+};
+
+const gcs = new Storage(config);
+// const bucket = storage.bucket('images-5d13c.appspot.com/');
+
 
 
 exports.generateThumbnail = functions.runWith({memory: '1GB', timeoutSeconds: '360'})
@@ -36,18 +59,32 @@ exports.generateThumbnail = functions.runWith({memory: '1GB', timeoutSeconds: '3
     let fileName = filePath.split('/').pop();
     // let bucketDir = dirname(filePath);
 
+    let imageKey = fileName+"_"+new Date().getTime().toString();
+
+
+    let firebaseData = {};
+
+    firebaseData['events'] = {
+      "system_assigned" : {
+        "description" : "",
+        "timestamp" : new Date().getTime()
+      }
+    }
+
+    const refImage = db.ref('log');
+
     let imageType = ['image','application']
 
     let contentType = object.contentType.split("/")[0]
 
     console.log("OBbject => "+object.contentType);
+    functions.logger.log('Before Image Size => '+ object.size+" Bytes");
 
     let bucketDir = "thumbnails";
 
     let workingDir = join(tmpdir(), 'thumbs');
     let tmpFilePath = join(workingDir, 'source.png');
 
-    // if (fileName.includes('thumb@') || !object.contentType.includes('image') || !object.contentType.includes('application/octet-stream')) {
     if (fileName.includes('thumb@') || imageType.indexOf(contentType) == -1) {
       console.log('exiting function');
       return false;
@@ -79,20 +116,27 @@ exports.generateThumbnail = functions.runWith({memory: '1GB', timeoutSeconds: '3
         format: 'JPEG',      // output format
         quality: 1           // the jpeg compression quality, between 0 and 1
       });
+      
       fileName=fileName.replace('heic','jpeg');
 
-    
       await promisify(fs.writeFile)(tmpFilePath, outputBuffer);
 
       
       
     }
-   
+
+
+
+   firebaseData['output'] = {}
+
+  //  let file['output'] = {}
 
     const uploadPromises = sizes.map(async size => {
 
       let thumbName = `thumb@${size}_${fileName}`;
       let thumbPath = join(workingDir, thumbName);
+
+      let start = new Date().getTime()
 
     // Sharp docs  https://sharp.pixelplumbing.com/api-resize
       await sharp(tmpFilePath)
@@ -104,14 +148,61 @@ exports.generateThumbnail = functions.runWith({memory: '1GB', timeoutSeconds: '3
       
         bucketDir = "thumbnails"+size.toString();
 
+        var fileSize = fs.statSync(thumbPath);
+        functions.logger.log(`After Image Size ${size} * ${size} =>` + fileSize.size+" Bytes");
+
+        let uploadPath = bucketDir+"/"+thumbName;
+
+        const thumbFile = bucket.file(uploadPath);
+       
+        await bucket.upload(thumbPath, {
+          destination: join(bucketDir, thumbName)
+        });
+
+
+      const results = await Promise.all([
+        thumbFile.getSignedUrl({
+          action: 'read',
+          expires: '03-01-2500',
+        })
+      ]);
+      functions.logger.log('Got Signed URLs.');
+      let thumbResult = results[0];
+      let thumbFileUrl = thumbResult[0];
+
+      console.log(size +' url => '+thumbFileUrl);
+
+        let end = new Date().getTime()
+
+
+        firebaseData['output'][size] = {
+          "start" : start,
+          'end' : end,
+          'elapsed' : end -start,
+          'bytes' : fileSize.size,
+          'urlAccess' : thumbFileUrl,
+          'status' : 'success',
+          'error' : 'no'
+        };
+
+        console.log("finished =>"+size);
+
       // Upload to GCS
-      return bucket.upload(thumbPath, {
-        destination: join(bucketDir, thumbName)
-      });
+      return size;
+
     });
 
     // 4. Run the upload operations
     await Promise.all(uploadPromises);
+
+
+    
+    imageKey = imageKey.replace('.','_');
+
+    const imageRef = refImage.child(imageKey);
+    await imageRef.set(firebaseData);
+
+    console.log("image uploaded to firebase");
 
     // 5. Cleanup remove the tmp/thumbs from the filesystem
     return fs.remove(workingDir);
